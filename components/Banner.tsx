f5868837a094818b8c81360d1e2219dc83de7f15
IamMrTeapot <mrteapot2016@gmail.com>
2023-01-19 19:36:38 +0700

    feat(frontend):  add banner

---
+++ components/Banner.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "@/styles/Banner.module.css";
+
+const Banner = () => {
+  return <div className={style.banner_container}>Banner</div>;
+};
+
+export default Banner;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Banner from "@/components/Banner";
 import Header from "@/components/Header";
 import { Inter } from "@next/font/google";
 
@@ -9,6 +10,7 @@ export default function Home() {
       {/* I just dont want to change the Body's property so I use another div instead. */}
       <div className="fullPage">
         <Header />
+        <Banner />
       </div>
     </>
   );
