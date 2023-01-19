c0ec043017482a69d630d2b6dd5354e920b409fa
IamMrTeapot <mrteapot2016@gmail.com>
2023-01-19 20:22:44 +0700

    feat(frontend): add banner content

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,8 +1,34 @@
 import React from "react";
 import style from "@/styles/Banner.module.css";
+import Image from "next/image";
+import img from "@/public/banner_pic.png";
 
 const Banner = () => {
-  return <div className={style.banner_container}>Banner</div>;
+  return (
+    <div className={style.banner}>
+      <div className={style.container}>
+        <div className={style.leftSide}>
+          <Image
+            src={img}
+            alt="Backpack"
+            width={32}
+            style={{ marginRight: "16px" }}
+          />
+          <p style={{ marginRight: "5px" }} className={style.bold_font}>
+            Travelling to Qatar?
+          </p>
+          <p className={style.light_font}>
+            Read all updated requirements before you book.
+          </p>
+        </div>
+        <div className={style.rightSide}>
+          <button className={`${style.light_font} ${style.button}`}>
+            Learn more
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
