0acac70a152f6a8365982015ce3c1dce5f2a7857
IamMrTeapot <mrteapot2016@gmail.com>
2023-01-19 18:34:37 +0700

    feat(frontend): add pointer to icons and topics in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,9 @@
 import style from "@/styles/Header.module.css";
 import Image from "next/image";
 import img from "@/public/agoda_logo_cropped.jpg";
+import img2 from "@/public/cart_icon.png";
+import img3 from "@/public/Bars.png";
+import img4 from "@/public/Bars_hover.png";
 
 const Header = () => {
   return (
@@ -22,7 +25,16 @@ const Header = () => {
       </div>
 
       {/* Header right */}
-      <div className={style.header_right}>right side</div>
+      <div className={style.header_right}>
+        <div className={style.flex}>
+          <div className={style.icon_container}>
+            <Image src={img2} alt="cart logo" width={24} placeholder="blur" />
+          </div>
+          <div className={style.icon_container}>
+            <Image src={img3} alt="more icon" width={24} placeholder="blur" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
